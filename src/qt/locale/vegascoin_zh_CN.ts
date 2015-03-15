44862bdf5e38ceaca3c6cb11bedae2fa0fe55abc
zongyii <chadgtr34@gmail.com>
2015-03-15 17:06:39 +0800

    fixed translation to Vegascoin Chinese
    vegascoin in Chinese

--- src/qt/locale/vegascoin_zh_CN.ts
+++ src/qt/locale/vegascoin_zh_CN.ts
@@ -5,12 +5,12 @@
     <message>
         <location filename="../forms/aboutdialog.ui" line="+14"/>
         <source>About VegasCoin</source>
-        <translation>关于弗兰克币</translation>
+        <translation>关于维加斯币</translation>
     </message>
     <message>
         <location line="+39"/>
         <source>&lt;b&gt;VegasCoin&lt;/b&gt; version</source>
-        <translation>&lt;b&gt;弗兰克币&lt;/b&gt;版本</translation>
+        <translation>&lt;b&gt;维加斯币&lt;/b&gt;版本</translation>
     </message>
     <message>
         <location line="+57"/>
@@ -68,7 +68,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location filename="../addressbookpage.cpp" line="+63"/>
         <source>These are your VegasCoin addresses for receiving payments. You may want to give a different one to each sender so you can keep track of who is paying you.</source>
-        <translation>这是您用来收款的弗兰克币地址。为了标记不同的资金来源，建议为每个付款人保留不同的收款地址。</translation>
+        <translation>这是您用来收款的维加斯币地址。为了标记不同的资金来源，建议为每个付款人保留不同的收款地址。</translation>
     </message>
     <message>
         <location filename="../forms/addressbookpage.ui" line="+14"/>
@@ -108,7 +108,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="-44"/>
         <source>Verify a message to ensure it was signed with a specified VegasCoin address</source>
-        <translation>验证消息，确保消息是由指定的弗兰克币地址签名过的。</translation>
+        <translation>验证消息，确保消息是由指定的维加斯币地址签名过的。</translation>
     </message>
     <message>
         <location line="+3"/>
@@ -123,7 +123,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location filename="../addressbookpage.cpp" line="-5"/>
         <source>These are your VegasCoin addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
-        <translation>这是您用来付款的弗兰克币地址。在付款前，请总是核实付款金额和收款地址。</translation>
+        <translation>这是您用来付款的维加斯币地址。在付款前，请总是核实付款金额和收款地址。</translation>
     </message>
     <message>
         <location line="+13"/>
@@ -249,7 +249,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="+1"/>
         <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR LITECOINS&lt;/b&gt;!</source>
-        <translation>警告：如果您加密了您的钱包，但是忘记了密码，你将会&lt;b&gt;丢失所有的弗兰克币&lt;/b&gt;！</translation>
+        <translation>警告：如果您加密了您的钱包，但是忘记了密码，你将会&lt;b&gt;丢失所有的维加斯币&lt;/b&gt;！</translation>
     </message>
     <message>
         <location line="+0"/>
@@ -276,7 +276,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="-56"/>
         <source>VegasCoin will close now to finish the encryption process. Remember that encrypting your wallet cannot fully protect your vegascoins from being stolen by malware infecting your computer.</source>
-        <translation>将关闭软件以完成加密过程。 请您谨记：钱包加密并不是万能的，电脑中毒，您的弗兰克币还是有可能丢失。</translation>
+        <translation>将关闭软件以完成加密过程。 请您谨记：钱包加密并不是万能的，电脑中毒，您的维加斯币还是有可能丢失。</translation>
     </message>
     <message>
         <location line="+13"/>
@@ -375,7 +375,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="+4"/>
         <source>Show information about VegasCoin</source>
-        <translation>显示弗兰克币的相关信息</translation>
+        <translation>显示维加斯币的相关信息</translation>
     </message>
     <message>
         <location line="+2"/>
@@ -420,7 +420,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="-347"/>
         <source>Send coins to a VegasCoin address</source>
-        <translation>向一个弗兰克币地址发送弗兰克币</translation>
+        <translation>向一个维加斯币地址发送维加斯币</translation>
     </message>
     <message>
         <location line="+49"/>
@@ -456,7 +456,7 @@ This product includes software developed by the OpenSSL Project for use in the O
         <location line="-165"/>
         <location line="+530"/>
         <source>VegasCoin</source>
-        <translation>弗兰克币</translation>
+        <translation>维加斯币</translation>
     </message>
     <message>
         <location line="-530"/>
@@ -481,7 +481,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="+22"/>
         <source>&amp;About VegasCoin</source>
-        <translation>&amp;关于弗兰克币</translation>
+        <translation>&amp;关于维加斯币</translation>
     </message>
     <message>
         <location line="+9"/>
@@ -501,12 +501,12 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="+7"/>
         <source>Sign messages with your VegasCoin addresses to prove you own them</source>
-        <translation>用弗兰克币地址关联的私钥为消息签名，以证明您拥有这个弗兰克币地址</translation>
+        <translation>用维加斯币地址关联的私钥为消息签名，以证明您拥有这个维加斯币地址</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Verify messages to ensure they were signed with specified VegasCoin addresses</source>
-        <translation>校验消息，确保该消息是由指定的弗兰克币地址所有者签名的</translation>
+        <translation>校验消息，确保该消息是由指定的维加斯币地址所有者签名的</translation>
     </message>
     <message>
         <location line="+28"/>
@@ -537,12 +537,12 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="+47"/>
         <source>VegasCoin client</source>
-        <translation>弗兰克币客户端</translation>
+        <translation>维加斯币客户端</translation>
     </message>
     <message numerus="yes">
         <location line="+141"/>
         <source>%n active connection(s) to VegasCoin network</source>
-        <translation><numerusform>到弗兰克币网络的连接共有%n条</numerusform></translation>
+        <translation><numerusform>到维加斯币网络的连接共有%n条</numerusform></translation>
     </message>
     <message>
         <location line="+22"/>
@@ -607,7 +607,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="+70"/>
         <source>This transaction is over the size limit. You can still send it for a fee of %1, which goes to the nodes that process your transaction and helps to support the network. Do you want to pay the fee?</source>
-        <translation>该交易的字节数超标。您可以选择支付%1的交易费给处理您的交易的网络节点，有助于弗兰克币网络的运行。您愿意支付这笔交易费用吗？</translation>
+        <translation>该交易的字节数超标。您可以选择支付%1的交易费给处理您的交易的网络节点，有助于维加斯币网络的运行。您愿意支付这笔交易费用吗？</translation>
     </message>
     <message>
         <location line="-140"/>
@@ -657,7 +657,7 @@ Address: %4
         <location line="-23"/>
         <location line="+23"/>
         <source>URI can not be parsed! This can be caused by an invalid VegasCoin address or malformed URI parameters.</source>
-        <translation>URI无法解析！原因可能是弗兰克币地址不正确，或者URI参数错误。</translation>
+        <translation>URI无法解析！原因可能是维加斯币地址不正确，或者URI参数错误。</translation>
     </message>
     <message>
         <location line="+17"/>
@@ -738,7 +738,7 @@ Address: %4
     <message>
         <location line="-5"/>
         <source>The entered address &quot;%1&quot; is not a valid VegasCoin address.</source>
-        <translation>您输入的 &quot;%1&quot; 不是合法的弗兰克币地址.</translation>
+        <translation>您输入的 &quot;%1&quot; 不是合法的维加斯币地址.</translation>
     </message>
     <message>
         <location line="+10"/>
@@ -821,7 +821,7 @@ Address: %4
     <message>
         <location line="+31"/>
         <source>Automatically start VegasCoin after logging in to the system.</source>
-        <translation>登录系统后自动开启弗兰克币客户端</translation>
+        <translation>登录系统后自动开启维加斯币客户端</translation>
     </message>
     <message>
         <location line="+3"/>
@@ -846,7 +846,7 @@ Address: %4
     <message>
         <location line="+6"/>
         <source>Automatically open the VegasCoin client port on the router. This only works when your router supports UPnP and it is enabled.</source>
-        <translation>自动在路由器中打开弗兰克币端口。只有当您的路由器开启 UPnP 选项时此功能才有效。</translation>
+        <translation>自动在路由器中打开维加斯币端口。只有当您的路由器开启 UPnP 选项时此功能才有效。</translation>
     </message>
     <message>
         <location line="+3"/>
@@ -856,7 +856,7 @@ Address: %4
     <message>
         <location line="+7"/>
         <source>Connect to the VegasCoin network through a SOCKS proxy (e.g. when connecting through Tor).</source>
-        <translation>通过代理服务器连接弗兰克币网络(例如：通过Tor连接)</translation>
+        <translation>通过代理服务器连接维加斯币网络(例如：通过Tor连接)</translation>
     </message>
     <message>
         <location line="+3"/>
@@ -936,22 +936,22 @@ Address: %4
     <message>
         <location line="+11"/>
         <source>&amp;Unit to show amounts in:</source>
-        <translation>&amp;弗兰克币金额单位:</translation>
+        <translation>&amp;维加斯币金额单位:</translation>
     </message>
     <message>
         <location line="+13"/>
         <source>Choose the default subdivision unit to show in the interface and when sending coins.</source>
-        <translation>选择弗兰克币单位。</translation>
+        <translation>选择维加斯币单位。</translation>
     </message>
     <message>
         <location line="+9"/>
         <source>Whether to show VegasCoin addresses in the transaction list or not.</source>
-        <translation>是否需要在交易清单中显示弗兰克币地址。</translation>
+        <translation>是否需要在交易清单中显示维加斯币地址。</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>&amp;Display addresses in transaction list</source>
-        <translation>在交易清单中&amp;显示弗兰克币地址</translation>
+        <translation>在交易清单中&amp;显示维加斯币地址</translation>
     </message>
     <message>
         <location line="+71"/>
@@ -1017,7 +1017,7 @@ Address: %4
         <location line="+50"/>
         <location line="+166"/>
         <source>The displayed information may be out of date. Your wallet automatically synchronizes with the VegasCoin network after a connection is established, but this process has not completed yet.</source>
-        <translation>现在显示的消息可能是过期的. 在连接上弗兰克币网络节点后，您的钱包将自动与网络同步，但是这个过程还没有完成.</translation>
+        <translation>现在显示的消息可能是过期的. 在连接上维加斯币网络节点后，您的钱包将自动与网络同步，但是这个过程还没有完成.</translation>
     </message>
     <message>
         <location line="-124"/>
@@ -1071,7 +1071,7 @@ Address: %4
     <message>
         <location filename="../paymentserver.cpp" line="+107"/>
         <source>Cannot start vegascoin: click-to-pay handler</source>
-        <translation>暂时无法启动弗兰克币：点击支付功能</translation>
+        <translation>暂时无法启动维加斯币：点击支付功能</translation>
     </message>
 </context>
 <context>
@@ -1186,7 +1186,7 @@ Address: %4
     <message>
         <location line="+23"/>
         <source>On testnet</source>
-        <translation>当前为弗兰克币测试网络</translation>
+        <translation>当前为维加斯币测试网络</translation>
     </message>
     <message>
         <location line="+23"/>
@@ -1241,12 +1241,12 @@ Address: %4
     <message>
         <location line="-104"/>
         <source>VegasCoin - Debug window</source>
-        <translation>弗兰克币 - 调试窗口</translation>
+        <translation>维加斯币 - 调试窗口</translation>
     </message>
     <message>
         <location line="+25"/>
         <source>VegasCoin Core</source>
-        <translation>弗兰克币核心</translation>
+        <translation>维加斯币核心</translation>
     </message>
     <message>
         <location line="+279"/>
@@ -1386,7 +1386,7 @@ Address: %4
     <message>
         <location line="+5"/>
         <source>Error: The transaction was rejected. This might happen if some of the coins in your wallet were already spent, such as if you used a copy of wallet.dat and coins were spent in the copy but not marked as spent here.</source>
-        <translation>错误: 交易被拒绝. 如果您使用的是备份钱包，可能存在两个钱包不同步的情况，另一个钱包中的弗兰克币已经被使用，但本地的这个钱包尚没有记录。</translation>
+        <translation>错误: 交易被拒绝. 如果您使用的是备份钱包，可能存在两个钱包不同步的情况，另一个钱包中的维加斯币已经被使用，但本地的这个钱包尚没有记录。</translation>
     </message>
 </context>
 <context>
@@ -1450,7 +1450,7 @@ Address: %4
     <message>
         <location filename="../sendcoinsentry.cpp" line="+1"/>
         <source>Enter a VegasCoin address (e.g. QcV4kzBs7ZivJR7QZvZCDvdvi6rmGVzJLL)</source>
-        <translation>请输入弗兰克币地址 (例如: QcV4kzBs7ZivJR7QZvZCDvdvi6rmGVzJLL)</translation>
+        <translation>请输入维加斯币地址 (例如: QcV4kzBs7ZivJR7QZvZCDvdvi6rmGVzJLL)</translation>
     </message>
 </context>
 <context>
@@ -1551,7 +1551,7 @@ Address: %4
     <message>
         <location line="+40"/>
         <source>Verify the message to ensure it was signed with the specified VegasCoin address</source>
-        <translation>验证消息，确保消息是由指定的弗兰克币地址签名过的。</translation>
+        <translation>验证消息，确保消息是由指定的维加斯币地址签名过的。</translation>
     </message>
     <message>
         <location line="+3"/>
@@ -1567,7 +1567,7 @@ Address: %4
         <location filename="../signverifymessagedialog.cpp" line="+27"/>
         <location line="+3"/>
         <source>Enter a VegasCoin address (e.g. QcV4kzBs7ZivJR7QZvZCDvdvi6rmGVzJLL)</source>
-        <translation>请输入弗兰克币地址 (例如: QcV4kzBs7ZivJR7QZvZCDvdvi6rmGVzJLL)</translation>
+        <translation>请输入维加斯币地址 (例如: QcV4kzBs7ZivJR7QZvZCDvdvi6rmGVzJLL)</translation>
     </message>
     <message>
         <location line="-2"/>
@@ -1577,7 +1577,7 @@ Address: %4
     <message>
         <location line="+3"/>
         <source>Enter VegasCoin signature</source>
-        <translation>输入弗兰克币签名</translation>
+        <translation>输入维加斯币签名</translation>
     </message>
     <message>
         <location line="+82"/>
@@ -1785,7 +1785,7 @@ Address: %4
     <message>
         <location line="+3"/>
         <source>Generated coins must mature 120 blocks before they can be spent. When you generated this block, it was broadcast to the network to be added to the block chain. If it fails to get into the chain, its state will change to &quot;not accepted&quot; and it won&apos;t be spendable. This may occasionally happen if another node generates a block within a few seconds of yours.</source>
-        <translation>新挖出的弗兰克币必须等确120个确认才能使用。您生产出的数据块，将被广播到全网并添加到数据块链。如果入链失败，状态将变为“未被接受”，意味着您的数据块竞争失败，挖出的弗兰克币将不能使用。当某个节点先于你几秒生产出新的数据块，这种情况会偶尔发生。</translation>
+        <translation>新挖出的维加斯币必须等确120个确认才能使用。您生产出的数据块，将被广播到全网并添加到数据块链。如果入链失败，状态将变为“未被接受”，意味着您的数据块竞争失败，挖出的维加斯币将不能使用。当某个节点先于你几秒生产出新的数据块，这种情况会偶尔发生。</translation>
     </message>
     <message>
         <location line="+7"/>
@@ -1946,7 +1946,7 @@ Address: %4
     <message>
         <location line="+2"/>
         <source>Date and time that the transaction was received.</source>
-        <translation>接收弗兰克币的时间</translation>
+        <translation>接收维加斯币的时间</translation>
     </message>
     <message>
         <location line="+2"/>
@@ -2138,7 +2138,7 @@ Address: %4
     <message>
         <location filename="../walletmodel.cpp" line="+193"/>
         <source>Send Coins</source>
-        <translation>发送弗兰克币</translation>
+        <translation>发送维加斯币</translation>
     </message>
 </context>
 <context>
@@ -2189,7 +2189,7 @@ Address: %4
     <message>
         <location filename="../vegascoinstrings.cpp" line="+94"/>
         <source>VegasCoin version</source>
-        <translation>弗兰克币版本</translation>
+        <translation>维加斯币版本</translation>
     </message>
     <message>
         <location line="+102"/>
@@ -2350,7 +2350,7 @@ rpcpassword=%s
     <message>
         <location line="+3"/>
         <source>Error: The transaction was rejected! This might happen if some of the coins in your wallet were already spent, such as if you used a copy of wallet.dat and coins were spent in the copy but not marked as spent here.</source>
-        <translation>错误：该交易被拒绝！发生这种错误的原因可能是：钱包中的弗兰克币已经被用掉，有可能您复制了wallet.dat钱包文件，然后用复制的钱包文件支付了弗兰克币，但是这个钱包文件中没有记录。</translation>
+        <translation>错误：该交易被拒绝！发生这种错误的原因可能是：钱包中的维加斯币已经被用掉，有可能您复制了wallet.dat钱包文件，然后用复制的钱包文件支付了维加斯币，但是这个钱包文件中没有记录。</translation>
     </message>
     <message>
         <location line="+4"/>
@@ -2390,7 +2390,7 @@ rpcpassword=%s
     <message>
         <location line="+3"/>
         <source>Warning: Please check that your computer&apos;s date and time are correct! If your clock is wrong VegasCoin will not work properly.</source>
-        <translation>警告：请检查电脑的日期时间设置是否正确！时间错误可能会导致弗兰克币客户端运行异常。</translation>
+        <translation>警告：请检查电脑的日期时间设置是否正确！时间错误可能会导致维加斯币客户端运行异常。</translation>
     </message>
     <message>
         <location line="+3"/>
@@ -2840,12 +2840,12 @@ rpcpassword=%s
     <message>
         <location line="+1"/>
         <source>Error loading wallet.dat: Wallet requires newer version of VegasCoin</source>
-        <translation>wallet.dat钱包文件加载错误：请升级到最新VegasCoin客户端</translation>
+        <translation>wallet.dat钱包文件加载错误：请升级到最新维加斯币客户端</translation>
     </message>
     <message>
         <location line="+93"/>
         <source>Wallet needed to be rewritten: restart VegasCoin to complete</source>
-        <translation>钱包文件需要重写：请退出并重新启动VegasCoin客户端</translation>
+        <translation>钱包文件需要重写：请退出并重新启动维加斯币客户端</translation>
     </message>
     <message>
         <location line="-95"/>
@@ -2905,7 +2905,7 @@ rpcpassword=%s
     <message>
         <location line="-25"/>
         <source>Unable to bind to %s on this computer. VegasCoin is probably already running.</source>
-        <translation>无法在本机绑定 %s 端口 . 弗兰克币客户端软件可能已经在运行.</translation>
+        <translation>无法在本机绑定 %s 端口 . 维加斯币客户端软件可能已经在运行.</translation>
     </message>
     <message>
         <location line="+64"/>
